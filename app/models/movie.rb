92513818a4f42cddbefda73eb13cbe87af3bedab
EC2 Default User <ec2-user@ip-172-31-33-101.us-east-2.compute.internal>
2021-02-26 01:47:36 +0000

    part 1+2.10

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ActiveRecord::Base
     def self.all_ratings
         return Movie.pluck('distinct(rating)')
     end
+    
     def self.with_ratings(ratings_list)
         if ratings_list == []
             return Movie.all
